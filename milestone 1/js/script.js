9cb1beb0ad5cb6370506be834758f246f980569a
MichaelScarantino <scarantino.michael@outlook.it>
2021-12-10 18:01:07 +0100

    Done Milestone 1

--- milestone 1/js/script.js
+++ milestone 1/js/script.js
@@ -1,8 +1,15 @@
+// Milestone 1
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) 
+// e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, 
+// visualizzare nome e immagine di ogni contatto
+
 Vue.config.devtools = true;
 const app = new Vue(
     {
         el: '#root',
         data: {
+            currentContactActive: 0,
             contacts: [
                 {
                     name: 'Michele',
@@ -87,7 +94,12 @@ const app = new Vue(
                         }
                     ],
                 },
-            ]
+            ],
+        },
+        methods: {
+            imageCurrentactive: function(index) {
+                this.currentContactActive = index
+            },
         }
     }
 );
